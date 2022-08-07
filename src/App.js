8613dd19d30cde464f1d3f30561874afcc891fef
Your Name <you@example.com>
2022-08-07 21:38:23 +0430

    use state in functional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./components/Product/Product";
 
-class App extends React.Component {
-    state = {
+const App = () => {
+    const [productState , setProductState] = useState({
         products: [
             {title : "book1", price : 50},
             {title : "book2", price : 80},
             {title : "book3", price : 70},
         ],
-}
-ChangeValue = () =>{
-    this.setState({
+})
+const ChangeValue = () =>{
+    setProductState({
         products: [
             {title : "book1", price : 30},
             {title : "book2", price : 30},
@@ -18,26 +18,24 @@ ChangeValue = () =>{
         ],
     })
 }
-    render(){
     return (
             <div id="main" className="container">
             <h1>React App</h1>
             <Product
-            title = {this.state.products[0].title}
-            price = {this.state.products[0].price}
+            title = {productState.products[0].title}
+            price = {productState.products[0].price}
             />
             <Product
-            title = {this.state.products[1].title}
-            price = {this.state.products[1].price}
+            title = {productState.products[1].title}
+            price = {productState.products[1].price}
             />
             <Product
-            title = {this.state.products[2].title}
-            price = {this.state.products[2].price}
+            title = {productState.products[2].title}
+            price = {productState.products[2].price}
             />
-            <button onClick={this.ChangeValue}> click me!</button>
+            <button onClick={ChangeValue}> click me!</button>
         </div>
     )
-}
 }
 
 export default App
