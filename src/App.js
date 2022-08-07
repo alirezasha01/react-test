11268e21f6df812e1aa177f6df3e4e4ae1fed7a9
Your Name <you@example.com>
2022-08-07 17:45:51 +0430

    JSX Learning

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () =>{
+    return (
+        <div id="main" className="container">
+            <h1>React App</h1>
+            <p>react react</p>
+        </div>
+    )
+}
+
+export default App
--- src/components/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const App = () =>{
-    return React.createElement("div" , {id : "main" , className:"container"},"hello world");
-}
-
-export default App
