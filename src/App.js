7b6aa2f97c032c4b6bdf37be82b7e572be9fd164
Your Name <you@example.com>
2022-08-07 18:46:50 +0430

    Using class component instead of functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Product from "./components/Product/Product";
 
-const App = () =>{
-    return (
-        <div id="main" className="container">
-            <h1>React App</h1>
-            <Product/>
-        </div>
-    )
-}
+// const App = () =>{
+//     return (
+//         <div id="main" className="container">
+//             <h1>React App</h1>
+//             <Product/>
+//         </div>
+//     )
+// }
 
+class App extends React.Component{
+    render(){
+        return(
+          <div id="main" className="container">
+              <h1>React App</h1>
+              <Product/>
+          </div>
+        )
+    }
+}
 export default App
