7e838b93d9ae3a8e13ac442c729473a7e12f4532
Alireza Shakouri <alirezasha01gmail.com>
2022-08-11 12:31:31 +0430

    use dynamic lists with loop

--- src/App.js
+++ src/App.js
@@ -1,44 +1,6 @@
 import React, { useState } from "react";
 import Product from "./components/Product/Product";
 import "./App.css"
-
-
-// const App = () => {
-//     const [productState , setProductState] = useState({
-//         products: [
-//             {title : "book1", price : 50},
-//             {title : "book2", price : 80},
-//             {title : "book3", price : 70},
-//         ],
-// })
-// const ChangeValue = () =>{
-//     setProductState({
-//         products: [
-//             {title : "book1", price : 30},
-//             {title : "book2", price : 30},
-//             {title : "book3", price : 30},
-//         ],
-//     })
-// }
-//     return (
-//             <div className="center">
-//             <h1>React App</h1>
-//             <Product
-//             title = {productState.products[0].title}
-//             price = {productState.products[0].price}
-//             />
-//             <Product
-//             title = {productState.products[1].title}
-//             price = {productState.products[1].price}
-//             />
-//             <Product
-//             title = {productState.products[2].title}
-//             price = {productState.products[2].price}
-//             />
-//             <button onClick={ChangeValue}> click me!</button>
-//         </div>
-//     )
-// }
 class App extends React.Component {
     state = {
         products: [
@@ -76,18 +38,11 @@ changeTogleHandler = () =>{
     if(this.state.showAndHide){
         nullProduct = (
             <div>
-            <Product
-            title = {this.state.products[0].title}
-            price = {this.state.products[0].price}
-            />
-            <Product
-            title = {this.state.products[1].title}
-            price = {this.state.products[1].price}
-            />
-            <Product
-            title = {this.state.products[2].title}
-            price = {this.state.products[2].price}
-            />
+            {
+                this.state.products.map((item)=>{
+                return <Product title={item.title} price={item.price} />
+            })
+            }
             </div>
         )
     }
