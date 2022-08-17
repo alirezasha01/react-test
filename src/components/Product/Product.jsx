40d6a1395dafedbca418b100f1749776de14b6fa
Alireza Shakouri <alirezasha01@gmail.com>
2022-08-18 01:12:31 +0430

    using ref in class component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,13 +3,20 @@ import "./Product.css"
 import Container from "../hoc/Container";
 import Wrapper from "../hoc/Wrapper";
 class Product extends React.Component{
+    constructor(props){
+        super(props)
+        this.inputRef = React.createRef()
+    }
+    componentDidMount(){
+        this.inputRef.current.focus()
+    }
     render(){
         console.log("Product")
         return(
             <Container>
-            <p key="1" onClick={this.props.click}>{this.props.title}</p>
-            <p key="2">{this.props.price}</p>
-            <input key="3" type="text" onChange={this.props.change} value={this.props.title}></input>
+            <p key="2" onClick={this.props.click}>{this.props.title}</p>
+            <p key="3">{this.props.price}</p>
+            <input ref={this.inputRef} key="1" type="text" onChange={this.props.change} value={this.props.title}></input>
             </Container>
         )
         }
