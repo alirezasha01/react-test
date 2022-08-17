3004acf42bd66ad6fdf3e27949ea3055d7a84fb9
Alireza Shakouri <alirezasha01@gmail.com>
2022-08-17 22:48:54 +0430

    using hoc(higher order component) instead of div

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Product.css"
-
+import Container from "../hoc/Container";
 class Product extends React.Component{
     render(){
         console.log("Product")
-        return[
-            <p key="1" onClick={this.props.click}>{this.props.title}</p>,
-            <p key="2">{this.props.price}</p>,
-            <input key="3" type="text" onChange={this.props.change} value={this.props.title}></input>,
-        ]
+        return(
+            <Container>
+            <p key="1" onClick={this.props.click}>{this.props.title}</p>
+            <p key="2">{this.props.price}</p>
+            <input key="3" type="text" onChange={this.props.change} value={this.props.title}></input>
+            </Container>
+        )
         }
 };
 
